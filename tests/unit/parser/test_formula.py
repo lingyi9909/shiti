3a8fb0b62cdf5bac765788d160864af588db7c2e
lingyi9909 <zhanhui9909@gmail.com>
2026-08-30 00:11:36 +0800

    style: format Task 2 formula tests

--- tests/unit/parser/test_formula.py
+++ tests/unit/parser/test_formula.py
@@ -13,19 +13,31 @@ def _math(body: str) -> ET.Element:
 
 def test_fraction_superscript_subscript_root_and_grouping_convert_to_latex() -> None:
     assert omml_to_latex(
-        _math("<m:f><m:num><m:r><m:t>a</m:t></m:r></m:num><m:den><m:r><m:t>b</m:t></m:r></m:den></m:f>")
+        _math(
+            "<m:f><m:num><m:r><m:t>a</m:t></m:r></m:num>"
+            "<m:den><m:r><m:t>b</m:t></m:r></m:den></m:f>"
+        )
     ) == r"\frac{a}{b}"
     assert omml_to_latex(
-        _math("<m:sSup><m:e><m:r><m:t>x</m:t></m:r></m:e><m:sup><m:r><m:t>2</m:t></m:r></m:sup></m:sSup>")
+        _math(
+            "<m:sSup><m:e><m:r><m:t>x</m:t></m:r></m:e>"
+            "<m:sup><m:r><m:t>2</m:t></m:r></m:sup></m:sSup>"
+        )
     ) == "x^{2}"
     assert omml_to_latex(
-        _math("<m:sSub><m:e><m:r><m:t>x</m:t></m:r></m:e><m:sub><m:r><m:t>1</m:t></m:r></m:sub></m:sSub>")
+        _math(
+            "<m:sSub><m:e><m:r><m:t>x</m:t></m:r></m:e>"
+            "<m:sub><m:r><m:t>1</m:t></m:r></m:sub></m:sSub>"
+        )
     ) == "x_{1}"
     assert omml_to_latex(
         _math("<m:rad><m:e><m:r><m:t>x+1</m:t></m:r></m:e></m:rad>")
     ) == r"\sqrt{x+1}"
     assert omml_to_latex(
-        _math("<m:d><m:dPr><m:begChr m:val='('/><m:endChr m:val=')'/></m:dPr><m:e><m:r><m:t>x+1</m:t></m:r></m:e></m:d>")
+        _math(
+            "<m:d><m:dPr><m:begChr m:val='('/><m:endChr m:val=')'/>"
+            "</m:dPr><m:e><m:r><m:t>x+1</m:t></m:r></m:e></m:d>"
+        )
     ) == "(x+1)"
 
 
